1af7307daa7d995f03bfee09bf79dee5d9df7fc5
Aailmz <mirzazubaridjunaid@gmail.com>
2024-11-04 20:59:04 +0700

    Add exit menu and loop before exit Golang #6 #7

--- caesarcipher.go
+++ caesarcipher.go
@@ -52,30 +52,35 @@ func bruteForceCaesar(ciphertext string) {
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Select option (1: Encrypt, 2: Solve): ")
-	choice, _ := reader.ReadString('\n')
-	choice = strings.TrimSpace(choice)
+	for {
+		fmt.Println("Select option (1: Encrypt, 2: Solve, 3: Exit): ")
+		choice, _ := reader.ReadString('\n')
+		choice = strings.TrimSpace(choice)
 
-	switch choice {
-	case "1":
-		fmt.Println("Input text: ")
-		plaintext, _ := reader.ReadString('\n')
-		plaintext = strings.TrimSpace(plaintext)
+		switch choice {
+		case "1":
+			fmt.Println("Input text: ")
+			plaintext, _ := reader.ReadString('\n')
+			plaintext = strings.TrimSpace(plaintext)
 
-		fmt.Println("Input shift value: ")
-		var shift int
-		fmt.Scan(&shift)
+			fmt.Println("Input shift value: ")
+			var shift int
+			fmt.Scan(&shift)
 
-		encrypted := encryptCaesar(plaintext, shift)
-		fmt.Printf("Result: %s\n", encrypted)
-	case "2":
-		fmt.Println("Input encrypted text: ")
-		ciphertext, _ := reader.ReadString('\n')
-		ciphertext = strings.TrimSpace(ciphertext)
+			encrypted := encryptCaesar(plaintext, shift)
+			fmt.Printf("Result: %s\n", encrypted)
+		case "2":
+			fmt.Println("Input encrypted text: ")
+			ciphertext, _ := reader.ReadString('\n')
+			ciphertext = strings.TrimSpace(ciphertext)
 
-		fmt.Println("Solving Encryption...")
-		bruteForceCaesar(ciphertext)
-	default:
-		fmt.Println("Not valid.")
+			fmt.Println("Solving Encryption...")
+			bruteForceCaesar(ciphertext)
+		case "3":
+			fmt.Println("Thank you for using Caesar Cipher. Goodbye!")
+			return
+		default:
+			fmt.Println("Not valid.")
+		}
 	}
 }
